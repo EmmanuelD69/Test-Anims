8fed95218edf13e0443a2e0ff4891cee6d2e2c28
EmmanuelD69 <emmanueldurouge@yahoo.com>
2020-03-06 13:44:41 +0100

    Corrections orthographe

--- logo fade in 2/js/app.js
+++ logo fade in 2/js/app.js
@@ -38,7 +38,7 @@ function aboutContent() {
   document.getElementById("paragraphe4").innerHTML =
     "A la suite de quoi mon parcours professionnel m'a mené dans la prestation de services où j'ai pu travailler aussi bien en mode BtC / BtB et découvrir des environnements divers, Nationaux et Internationaux. <br> J'ai collaboré au sein d'entreprises tels que: <br> ERDF / GRDF / LA POSTE / VEOLIA / DALKIA / MERIAL / SANOFI ...";
   document.getElementById("paragraphe5").innerHTML =
-    "J'ai pu aussi assouvir l'une de mes passions, le Djing, en travaillant dans le monde de la nuit dans des bars et boites de nuits sur Avignon, Lyon, et St Etienne.";
+    "J'ai pu aussi assouvir l'une de mes passions, le Djing, en travaillant dans des bars et boites de nuits sur Avignon, Lyon, et St Etienne.";
   document.getElementById("paragraphe6").innerHTML =
     "Très tôt, j'ai développé un intérêt pour la musique, le gaming et la communication sous toutes ses formes.";
 }
@@ -50,10 +50,10 @@ function hobbiesContent() {
     "Mon premier ordinateur fût un IBM Amstrad PC1512 et depuis la fin des années 80, j'ai passé d'interminables heures sur des jeux comme: <br> -Civilization <br> -Elite Dangerous <br> -Anarchy online <br> -World of Warcraft <br> -Ark online <br> -Second Life <br> pour n'en citer que quelques uns…  <br> J'ai développé très jeune une forte appétence pour le Gaming, que je pratique au quotidien.";
   document.getElementById("subtitle2").innerHTML = "DJING";
   document.getElementById("paragraphe2").innerHTML =
-    "Thriller - Michael Jackson a été mon premier achat, et reste pour moi le meilleur album que j'ai pu acheter. J'ai écouté la radio très tôt et enregistré mes premières cassettes, m'amusant à les couper / coller pour faire des enchainements… cela n'est qu'à partir de 1993 que j'ai acheté mes premières platines et vinyles accompagnés d'une table de mixage Numark et d'un gros caisson de basse à moitié moisi mais qui tapait fort :D";
+    "Thriller - Michael Jackson a été mon premier achat, et reste pour moi le meilleur album de Pop music de tous les temps. J'ai écouté la radio très tôt et enregistré mes premières cassettes, m'amusant à les couper / coller pour faire des enchainements… cela n'est qu'à partir de 1993 que j'ai acheté mes premières platines et vinyles accompagnés d'une table de mixage Numark et d'un gros caisson de basse à moitié moisi mais qui tapait fort :D";
   document.getElementById("subtitle3").innerHTML = "";
   document.getElementById("paragraphe3").innerHTML =
-    "Le mix et DJing est devenu une véritable passion que je continu de pratiquer fréquemment à titre personnel ou occasionnellement lors de soirées / évènements dans des pubs / nightclubs.";
+    "DJing est devenu une véritable passion que je continue de pratiquer fréquemment à titre personnel ou occasionnellement lors de soirées / évènements dans des pubs / nightclubs.";
   document.getElementById("subtitle4").innerHTML = "";
   document.getElementById("paragraphe4").innerHTML =
     'J\'ai un mixcloud que j\'utilise de temps en temps, et où l\'on peux écouter certains de mes mix <br> <a href="https://www.mixcloud.com/emmanuel-durouge" target="_blank">cliquez ici</a> pour acceder à mon Mixcloud.';
@@ -68,10 +68,10 @@ function iLikeContent() {
     "C'est simple, j'adore l'histoire, peu importe l'époque, j'ai toujours été intéressé par le sujet. Géopolitique, conquêtes, civilisations, cultures du monde, peu importe le sujet, je dévore avec grand plaisir les documentaires, articles, revues qui me passent à portée.";
   document.getElementById("subtitle2").innerHTML = "COMMUNICATION";
   document.getElementById("paragraphe2").innerHTML =
-    "CQ, CQ, CQ DX.... <br> CQ veut dire un appel général à toutes stations (provient de l'anglais Seek You --> je vous recherche), la mention DX spécifie des stations longue distance. <br> J'ai eu la chance de lancer des appels sur les ondes longue distance (communications radio) à la fin des années 90 et jusqu'au milieu des années 2000, établissant de très beaux contacts humain avec les quatre coins du monde. Un très bon moyen de développer son anglais! <br> <p>Depuis l'arrivé d'internet, c'est au travers de médias comme Facebook, Mixcloud, Discord, Twitch…. que je communique.</p>";
+    "CQ, CQ, CQ DX.... <br> CQ veut dire un appel général à toutes stations (provient de l'anglais Seek You --> je vous recherche), la mention DX spécifie des stations longues distances. <br> J'ai eu la chance de lancer des appels sur les ondes longues distances (communications radio) à la fin des années 90 et jusqu'au milieu des années 2000, établissant de très beaux contacts humains avec les quatre coins du monde. Un très bon moyen de développer son anglais! <br> <p>Depuis l'arrivée d'internet, c'est au travers des médias comme Facebook, Mixcloud, Discord, Twitch…. que je communique.</p>";
   document.getElementById("subtitle3").innerHTML = "MUSIC SURF";
   document.getElementById("paragraphe3").innerHTML =
-    "Le Djing étant l'une de mes passions, je ne peux rester sans découvrir de nouveaux artistes ou re découvrir les standard qui ont fait danser des générations entières. <br> Je passe donc souvent du temps à rechercher les pépites à venir, notamment en ce qui concerne la musique électronique qui reste mon style favori. <br> Comme chez un disquaire, je navigue sur les sites web de musique à la recherche des hits à venir, des nouveaux talents de la scène électro et l'inspiration pour mes futur mixes.";
+    "Le Djing étant l'une de mes passions, je ne peux rester sans découvrir de nouveaux artistes ou re découvrir les standards qui ont fait danser des générations entières. <br> Je passe donc souvent du temps à rechercher les pépites à venir, notamment en ce qui concerne la musique électronique qui reste mon style favori. <br> Comme chez un disquaire, je navigue sur les sites web de musique à la recherche des hits à venir, des nouveaux talents de la scène électro et l'inspiration pour mes futurs mixes.";
   document.getElementById("paragraphe4").innerHTML = "";
   document.getElementById("paragraphe5").innerHTML = "";
   document.getElementById("paragraphe6").innerHTML = "";
